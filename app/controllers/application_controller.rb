1539dbe059f38afe42d9d8046542a2eb206389dc
Steven Cassidy <steven@mumsnet.com>
2015-07-27 10:29:40 -0400

    Comment out before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :ensure_current_user
+  #before_filter :ensure_current_user
 
   def current_user
     puts "Session user is #{session[:user_id]}"
