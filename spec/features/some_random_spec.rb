a75a02d44f0192507b8242e014d086b40a4187a5
Steven Cassidy <steven@mumsnet.com>
2015-07-27 12:36:36 -0400

    Add topic display test

--- spec/features/some_random_spec.rb
+++ spec/features/some_random_spec.rb
@@ -2,11 +2,23 @@ require 'rails_helper'
 
 describe "the topic page", :type => :feature do
 
+  let! (:topic) { Topic.create!(name:'Steven')}
+  let! (:test_user) { User.create!(username:'steven', email:'steven@example.com', password:'12345')}
+
   before :each do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(test_user)
   end
 
   it "shows a list of topics" do
     visit '/'
-    expect(page).to have_content 'Topics'
+    expect(page).to have_content topic.name
   end
+
+  it "includes clickable topics" do
+    visit '/'
+    click_link topic.name
+    expect(page).to have_selector('[data-purpose="topic_name"]')
+  
+  end
+
 end
