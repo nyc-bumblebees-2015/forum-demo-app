8691785a11ca6c6f3f87299bd43c2124f065c8bf
Steven Cassidy <steven@mumsnet.com>
2015-07-27 10:47:00 -0400

    Add capybara DSL

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,9 +28,9 @@ require 'capybara/rspec'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Capybara::DSL
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
